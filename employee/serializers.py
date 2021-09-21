a6a4b6f2b4d190edbf995adbd15a9e3effc959b2
safwanvk <safwanvalakundil@gmail.com>
2021-09-21 19:58:00 +0530

    :sparkles: Setup employee serializer

---
+++ employee/serializers.py
@@ -0,0 +1,9 @@
+# Serializers define the API representation.
+from rest_framework import serializers
+
+from .models import Employee
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = "__all__"
